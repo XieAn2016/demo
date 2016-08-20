b02fc5631b4693084d510084d2921c255f33c1cb
XieJing2016 <xjysyy@gmail.com>
2016-08-20 23:03:27 +0800

    angelurjs继续

---
+++ angelurjs/app/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lenovo on 2016/8/20.
+ */
+var myApp = angular.module("myApp",[]);
+
+myApp.controller("firstController",["$scope",function($scope){
+    var userdata = {};
+
+}]);
